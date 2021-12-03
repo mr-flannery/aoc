a09edccedd9797e9edd080043090e5db0252dc30
Dennis Hempfing <dennis.hempfing@sap.com>
2021-12-03 17:02:36 +0100

    Add solution for day3 part2

--- 2021/src/main/clojure/day3/solution.clj
+++ 2021/src/main/clojure/day3/solution.clj
@@ -23,6 +23,7 @@
 (defn transpose [m]
   (apply mapv vector m))
 ;; is equivalent to (mapv vector ...m)
+;; apply is similar from a use case perspective like the spread operator in JS
 ;; map (and mapv) can operate on multiple collections at the same time
 ;; assuming f is an fn that takes an arbitrary number of args
 ;; works: (map + [1 2 3] [1 2 3])
@@ -36,18 +37,52 @@
 
 (defn selector-factory
   [comp-fn]
-  (fn [freq] (if (comp-fn (get freq \0) (get freq \1))
-               "0"
-               "1")))
+  (fn [freq] (if (comp-fn (get freq \1) (get freq \0))
+               "1"
+               "0")))
 
 (def select-gamma-bit (selector-factory >))
 (def select-epsilon-bit (selector-factory <))
 
-(defn -main
-  []
+(defn to-int
+  [bits]
+  (Integer/parseInt (apply str bits) 2))
+
+(defn part1
+  [input]
   (let [freqs (freqs-for-pos input)
         gamma-bits (map select-gamma-bit freqs)
         epsilon-bits (map select-epsilon-bit freqs)]
-    (* (Integer/parseInt (apply str gamma-bits) 2) (Integer/parseInt (apply str epsilon-bits) 2))))
+    (* (to-int gamma-bits) (to-int epsilon-bits))))
 
-(println (-main))
+(def select-o2-bit (selector-factory >=))
+(def select-co2-bit (selector-factory <))
+
+;; this function looks waaay to complex
+(defn select-bits
+  [input select-fn]
+  (let [i-max (dec (count (first input)))]
+    (loop [input input
+           result ""
+           i 0]
+      (let [freq (frequencies (nth (transpose input) i))
+            next-bit (select-fn freq)
+            mask (str result next-bit)
+            candidates (filter #(str/starts-with? % mask) input)]
+        (cond
+          (= i i-max) mask
+          (= (count candidates) 1) (first candidates)
+          :else (recur candidates mask (inc i)))))))
+
+(defn part2
+  [input]
+  (let [o2-bits (select-bits input select-o2-bit)
+        co2-bits (select-bits input select-co2-bit)]
+    (* (to-int o2-bits) (to-int co2-bits))))
+
+(defn -main
+  []
+  (println (part1 input)) ;; 3985686
+  (println (part2 input))) ;; 2555739
+
+(-main)
