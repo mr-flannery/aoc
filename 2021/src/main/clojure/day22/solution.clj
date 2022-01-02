14bdaba171b50091e49bea8235c24debbc956f57
Dennis Hempfing <dennis.hempfing@sap.com>
2022-01-02 11:13:36 +0100

    Add solution for day22

--- 2021/src/main/clojure/day22/solution.clj
+++ 2021/src/main/clojure/day22/solution.clj
@@ -107,8 +107,9 @@
           [x1 x2 x3 x4] (sort [ax1 ax2 bx1 bx2])
           [y1 y2 y3 y4] (sort [ay1 ay2 by1 by2])
           [z1 z2 z3 z4] (sort [az1 az2 bz1 bz2])]
-    {:x1 x2, :x2 x3, :y1 y2, :y2 y3, :z1 z2, :z2 z3 :op op2})))
+      {:x1 x2, :x2 x3, :y1 y2, :y2 y3, :z1 z2, :z2 z3 :op op2})))
 
+; nice idea, but scales horribly
 (defn split
   [{ax1 :x1 ax2 :x2 ay1 :y1 ay2 :y2 az1 :z1 az2 :z2 op1 :op}
    {bx1 :x1 bx2 :x2 by1 :y1 by2 :y2 bz1 :z1 bz2 :z2 op2 :op}]
@@ -137,7 +138,7 @@
 
 ;; danke Stephan
 (defn substract
-  "Returns what is left of cube1 after removing the space taken by cube2.
+  "Returns what is left of cube1 after removing the space taken by cube2. Empty cubes are removed.
    Returns an empty vector if cube2 encompasses cube1.
    Return a vector containing exactly cube1 if the cubes don't overlap."
   [cube1 cube2]
@@ -147,127 +148,36 @@
     :else (let [{ax1 :x1 ax2 :x2 ay1 :y1 ay2 :y2 az1 :z1 az2 :z2 op :op} cube1
                 {bx1 :x1 bx2 :x2 by1 :y1 by2 :y2 bz1 :z1 bz2 :z2}        cube2
                 {ox1 :x1 ox2 :x2 oy1 :y1 oy2 :y2 oz1 :z1 oz2 :z2}        (overlap cube1 cube2)]
-            [{:x1 (min ax1 ox1), :x2 ox1, :y1 ay1, :y2 ay2, :z1 az1, :z2 az2 :op op} ; links
-             {:x1 ox2, :x2 (max ax2 ox2), :y1 ay1, :y2 ay2, :z1 az1, :z2 az2 :op op} ; rechts
-             {:x1 ax1, :x2 ax2, :y1 (min ay1 oy1), :y2 oy1, :z1 az1, :z2 az2 :op op} ; unten
-             {:x1 ax1, :x2 ax2, :y1 oy2, :y2 (max ay2 oy2), :z1 az1, :z2 az2 :op op} ; oben
-             {:x1 ax1, :x2 ax2, :y1 ay1, :y2 ay2, :z1 (min az1 oz1), :z2 oz1 :op op} ; vorne
-             {:x1 ax1, :x2 ax2, :y1 ay1, :y2 ay2, :z1 oz2, :z2 (max az2 oz2) :op op} ; hinten
-             ])))
+            ;; same continuous vs discrete problem, I'm probably double-allotting cubes
+            (->> [{:x1 (min ax1 ox1), :x2 (dec ox1), :y1 ay1, :y2 ay2, :z1 az1, :z2 az2 :op op} ; links
+                  {:x1 (inc ox2), :x2 (max ax2 ox2), :y1 ay1, :y2 ay2, :z1 az1, :z2 az2 :op op} ; rechts
+                  {:x1 ox1, :x2 ox2, :y1 ay1, :y2 ay2, :z1 (min az1 oz1), :z2 (dec oz1) :op op} ; vorne
+                  {:x1 ox1, :x2 ox2, :y1 ay1, :y2 ay2, :z1 (inc oz2), :z2 (max az2 oz2) :op op} ; hinten
+                  {:x1 ox1, :x2 ox2, :y1 (min ay1 oy1), :y2 (dec oy1), :z1 oz1, :z2 oz2 :op op} ; unten
+                  {:x1 ox1, :x2 ox2, :y1 (inc oy2), :y2 (max ay2 oy2), :z1 oz1, :z2 oz2 :op op} ; oben
+                  ]
+                 (filter #(> (volume %) 0))))))
 
-; no overlap
-(substract {:x1 1, :x2 3, :y1 1, :y2 3, :z1 1, :z2 3} {:x1 4 :x2 5, :y1 4 :y2 5, :z1 4 :z2 5})
-; encompasses
-(substract {:x1 2, :x2 3, :y1 2, :y2 3, :z1 2, :z2 3} {:x1 1, :x2 4, :y1 1, :y2 4, :z2 4, :z1 1})
-(pprint (substract {:x1 1, :x2 4, :y1 1, :y2 4, :z1 1, :z2 4} {:x1 2, :x2 3, :y1 2, :y2 3, :z1 2, :z2 3}))
-(pprint (substract {:x1 1, :x2 4, :y1 1, :y2 4, :z1 1, :z2 4} {:x1 0, :x2 3, :y1 2, :y2 3, :z1 2, :z2 3}))
-(pprint (substract {:x1 1, :x2 4, :y1 1, :y2 4, :z1 1, :z2 4} {:x1 0, :x2 5, :y1 2, :y2 3, :z1 2, :z2 3}))
-(pprint (substract {:x1 1, :x2 4, :y1 1, :y2 4, :z1 1, :z2 4} {:x1 0, :x2 5, :y1 1, :y2 4, :z1 2, :z2 3}))
-
+;; volume is continuous, but cube count should be discrete...
 (defn volume
   [{x1 :x1 x2 :x2 y1 :y1 y2 :y2 z1 :z1 z2 :z2}]
-  (* (- x2 x1) (- y2 y1) (- z2 z1)))
+  (* (- x2 (dec x1)) (- y2 (dec y1)) (- z2 (dec z1))))
 
-(defn process-cubes
-  [new-cube existing-cube]
-  (if (overlap? new-cube existing-cube)
-    (->> (split new-cube existing-cube) (filter #(> (volume %) 0)))
-    [existing-cube] ; must always return a collection
-    ))
+(defn total-volume
+  [cubes]
+  (->> cubes
+       (map volume)
+       (reduce +)))
 
 (defn cube-reducer
   [all-cubes new-cube]
-  (println (count all-cubes))
-  (if (some #(overlap? new-cube %) all-cubes)
-    (mapcat #(process-cubes new-cube %) all-cubes)
-    (conj all-cubes new-cube)))
+  (let [subbed-cubes (mapcat #(substract % new-cube) all-cubes)]
+    (if (new-cube :op)
+      (conj subbed-cubes new-cube)
+      subbed-cubes)))
 
-;; nice idea, but scales horribly
-(->> (take 11 example-instructions)
+(->> real-instructions
      (reduce cube-reducer [])
      (map volume)
      (reduce +)
-     time)
-
-(defn symmetric-unique-pairs
-  [n]
-  (mapcat identity (for [a (range n)]
-                     (for [b (range (inc a) n)]
-                       [a b]))))
-
-(defn interval-encompasses?
-  [[a b] [c d]]
-  ;; (or
-  ;;  (and
-  ;;   (<= a c)
-  ;;   (>= b d))
-  ;;  (and
-  ;;   (<= c a)
-  ;;   (>= d b)))
-  (and
-   (<= a c)
-   (>= b d))
-  )
-
-(defn encompasses?
-  [{ax1 :x1 ax2 :x2 ay1 :y1 ay2 :y2 az1 :z1 az2 :z2 op1 :op}
-   {bx1 :x1 bx2 :x2 by1 :y1 by2 :y2 bz1 :z1 bz2 :z2 op2 :op}]
-  (and
-   (interval-encompasses? [ax1 ax2] [bx1 bx2])
-   (interval-encompasses? [ay1 ay2] [by1 by2])
-   (interval-encompasses? [az1 az2] [bz1 bz2])))
-
-(let [overlaps (for [[i1 i2] (symmetric-unique-pairs 5)]
-                 (let [cube1   (nth example-instructions i1)
-                       cube2   (nth example-instructions i2)]
-                   (when (overlap? cube1 cube2)
-                     (overlap cube1 cube2))))]
-  (encompasses? (nth overlaps 0) (nth overlaps 1)))
-
-
-
-(let [cube1   (nth example-instructions 0)
-      cube2   (nth example-instructions 1)
-      total-volume  (volume cube1)]
-  (if (overlap? cube1 cube2)
-    (let [ol (overlap cube1 cube2)]
-      (+ total-volume (volume cube1) (volume cube2) (- (volume ol))))))
-
-(let [cube1   (nth example-instructions 0)
-      cube2   (nth example-instructions 1)
-      cube3   (nth example-instructions 2)
-      cube4   (nth example-instructions 3)
-      total-volume  331548
-      [ol1 ol2 ol3] (map [cube1 cube2 cube3])]
-  (encompasses? ol3 ol2))
-
-
-(let [existing-cubes (take 19 example-instructions)
-      new-cube       (nth example-instructions 19)
-      overlaps       (->> existing-cubes
-                          (map #(overlap new-cube %))
-                          (filter some?))
-      encompassments (for [i1 (range 0 (count overlaps))
-                           i2 (range 0 (count overlaps))
-                           :when (not= i1 i2)]
-                       [[(nth overlaps i1) (nth overlaps i2)] (encompasses? (nth overlaps i1) (nth overlaps i2))])
-      encompassed-ols         (->> encompassments
-                                   (filter second)
-                                   (map first)
-                                   (map second)
-                                   set)
-      overlaps2 (remove encompassed-ols overlaps)
-      splits (mapcat (fn [[a b]] (split a b)) (partition 2 1 overlaps2))]
-  ;; (pprint (for [[i1 i2] (symmetric-unique-pairs (count overlaps2))]
-  ;;   [[i1 i2] (overlap (nth overlaps2 i1) (nth overlaps2 i2))]))  
-
-  ;; (reduce overlap overlaps2)
-
-  (->> overlaps2
-       (map :x1)
-       sort)
-  (->> overlaps2
-       (map :x2)
-       sort))
-
-[-41 7] [-22 9]
+     time) ;; 1257350313518866
