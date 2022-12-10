397fcbe75ac83c0496094423d8b51ae1cb5928d9
Dennis Hempfing <dennis.hempfing@sap.com>
2022-12-10 17:44:00 +0100

    Add solution for day 10 part 1

---
+++ 2022/src/main/clojure/day10/solution.clj
@@ -0,0 +1,48 @@
+(ns day10.solution
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.pprint :refer [pprint]])
+  (:import (java.util HashSet)))
+
+(def input (slurp (io/resource "day10/input.txt")))
+
+(def sample-input "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop\n")
+
+(defn parse-instructions
+  [input]
+  (->> (str/split-lines input)
+       (map (fn [s]
+              (let [match (re-find #"addx (-?\d+)" s)]
+                (if (some? match)
+                  (Integer/parseInt (second match))
+                  0))))
+       (reduce
+         (fn [nums num]
+           (if (zero? num)
+             (concat nums [num])
+             (concat nums [0 num]))) ; pad addx instructions to account for taking two cycles
+         ; 0 must come first, otherwise the result would materialize one cycle too early 
+         [])
+       (concat [1])))
+
+(def sample-instructions (parse-instructions sample-input))
+
+(def instructions (parse-instructions input))
+
+(defn signal-strength
+  [instructions cycle]
+  (* cycle (->> instructions
+                (take cycle)
+                (reduce +))))
+
+(defn register-value
+  [instructions cycle]
+  (->> instructions
+       (take cycle)
+       (reduce +)))
+
+(let [sample-at [20 60 100 140 180 220]]
+  (->> sample-at
+       (map #(signal-strength instructions %))
+       (reduce +)))
+
