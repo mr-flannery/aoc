7b69ec211bc62e437ba46419c061476c9c8044ee
Dennis Hempfing <dennis.hempfing@sap.com>
2022-12-17 23:50:15 +0100

    hardcore stateful version

--- 2022/src/main/clojure/day17/solution.clj
+++ 2022/src/main/clojure/day17/solution.clj
@@ -67,17 +67,21 @@
        (map (fn [[x y]] [(inc x) y]))
        (into #{})))
 
+(defn collides-with-pile?
+  [^HashSet pile piece]
+  (reduce #(or %1 %2) (map #(.contains pile %) piece)))
+
 (defn apply-jet
   [jet pile piece]
   (if (= jet \<)
     (if (and
           (not= (->> piece (map first) (apply min)) 0)
-          (= 0 (count (intersection (pile->set pile) (jet-left piece)))))
+          (not (collides-with-pile? pile (jet-left piece))))
       (jet-left piece)
       piece)
     (if (and
           (not= (->> piece (map first) (apply max)) 6)
-          (= 0 (count (intersection (pile->set pile) (jet-right piece)))))
+          (not (collides-with-pile? pile (jet-right piece))))
       (jet-right piece)
       piece)))
 
@@ -91,46 +95,64 @@
 (defn can-fall?
   [pile piece]
   (and
-    (= 0 (count (intersection (pile->set pile) (fall piece))))
+    (not (collides-with-pile? pile (fall piece)))
     (not= 0 (->> piece (map second) (apply min)))))
 
 ; this probably also causes scaling issues
 ; idea: also try take 10 here
 ; the max should be in the first ten
-(defn top-of-pile
-  [pile]
-  (if (not= 0 (count pile))
-    (->> pile (reduce union) (map second) (apply max))
-    -1))
+;(defn top-of-pile
+;  [pile]
+;  (if (not= 0 (count pile))
+;    (->> pile (reduce union) (map second) (apply max))
+;    -1))
 
 (defn next-piece
-  [piece-fn pile]
-  (piece-fn (+ 4 (top-of-pile pile))))
+  [piece-fn max-y]
+  (piece-fn (+ 4 max-y)))
 
 ; rotate input is way too expensive anyway
 (def sample-inputv (into [] sample-input))
 (def inputv (into [] input))
 
 (time
-  (->> (let [jets sample-inputv]
-         (loop [pile             '()
-                piece-fn-counter 0
+  (->> (let [jets  sample-inputv
+             pile  (HashSet.)
+             max-y (atom -1)]
+         (loop [piece-fn-counter 0
                 jet-counter      0
-                piece            (next-piece (get piece-fns piece-fn-counter) pile)]
-           (if (= (count pile)
+                piece-counter    0
+                piece            (next-piece (get piece-fns piece-fn-counter) @max-y)]
+           (if (= 0 (mod piece-counter 10000))
+             (println piece-counter))
+           (if (= piece-counter
                   ;1000000000000
+                  ;100000
                   2022
+                  ;10
                   )
-             pile
+             (inc @max-y) ; the task seems to start counting at 1, not at 0
              (let [jetted-piece (apply-jet (get jets jet-counter) pile piece)]
+               ;(println (sort piece))
+               ;(println (get jets jet-counter))
+               ;(println (sort jetted-piece))
                (if (can-fall? pile jetted-piece)
-                 (recur pile piece-fn-counter (mod (inc jet-counter) (count jets)) (fall jetted-piece))
-                 (let [new-pile             (conj pile jetted-piece)
-                       new-piece-fn-counter (mod (inc piece-fn-counter) 5)
-                       new-piece            (next-piece (get piece-fns new-piece-fn-counter) new-pile)]
-                   (recur new-pile new-piece-fn-counter (mod (inc jet-counter) (count jets)) new-piece)))))))
-       (reduce union)
-       (map second)
-       (apply max)
-       inc ; the task seems to start counting at 1, not at 0
+                 (recur piece-fn-counter (mod (inc jet-counter) (count jets)) piece-counter (fall jetted-piece))
+                 (do
+                   ;(println "zack")
+                   (.addAll pile jetted-piece)
+                   (let [max-piece-y (->> jetted-piece (map second) (apply max))]
+                     ;(println max-piece-y " > " @max-y "?")
+                     (if (> max-piece-y @max-y)
+                       (do 
+                         ;(println "swap!") 
+                         (swap! max-y (fn [_] max-piece-y)))))
+                   (let [new-piece-fn-counter (mod (inc piece-fn-counter) 5)
+                         new-piece            (next-piece (get piece-fns new-piece-fn-counter) @max-y)]
+                     (recur new-piece-fn-counter (mod (inc jet-counter) (count jets)) (inc piece-counter) new-piece))
+                   ))))))
+       ;(reduce union)
+       ;(map second)
+       ;(apply max)
+       ;inc ; the task seems to start counting at 1, not at 0
        ))
