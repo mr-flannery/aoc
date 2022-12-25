59f55c249a0bc18c05c1d30ff3f1fcdfcf1c32b5
Dennis Hempfing <dennis.hempfing@sap.com>
2022-12-25 16:49:05 +0100

    Add solution for day23

--- 2022/src/main/clojure/day23/solution.clj
+++ 2022/src/main/clojure/day23/solution.clj
@@ -8,37 +8,45 @@
 (def input (slurp (io/resource "day23/input.txt")))
 
 (def sample-input "..............\n..............\n.......#......\n.....###.#....\n...#...#.#....\n....#...##....\n...#.###......\n...##.#.##....\n....#..#......\n..............\n..............\n..............\n")
+(def mini-sample-input ".....\n..##.\n..#..\n.....\n..##.\n.....")
 
 (defn parse
   [input]
   (let [lines (str/split-lines input)]
-    (->> (for [i (range 0 (count lines))
-               j (range 0 (count (first lines)))]
-           (if (= \# (-> lines (nth i) (nth j)))
-             [j i])) ; switch j i to get proper x y coords
+    (->> (for [l (range 0 (count lines))
+               c (range 0 (count (first lines)))]
+           (if (= \# (-> lines (nth l) (nth c)))
+             [l c]))
          (filter identity)
          (into #{}))))
 
 (def sample-elves (parse sample-input))
+(def mini-sample-elves (parse mini-sample-input))
 (def elves (parse input))
 
 (defn north-neighbors
-  [[x y]]
-  [[(dec x) (dec y)] [x (dec y)] [(inc x) (dec y)]])
+  [[l c]]
+  [[(dec l) (dec c)] [(dec l) c] [(dec l) (inc c)]])
 
 (defn south-neighbors
-  [[x y]]
-  [[(dec x) (inc y)] [x (inc y)] [(inc x) (inc y)]])
+  [[l c]]
+  [[(inc l) (dec c)] [(inc l) c] [(inc l) (inc c)]])
 
 (defn east-neighbors
-  [[x y]]
-  [[(inc x) (dec y)] [(inc x) y] [(inc x) (inc y)]])
+  [[l c]]
+  [[(dec l) (inc c)] [l (inc c)] [(inc l) (inc c)]])
 
 (defn west-neighbors
-  [[x y]]
-  [[(dec x) (dec y)] [(dec x) y] [(dec x) (inc y)]])
+  [[l c]]
+  [[(dec l) (dec c)] [l (dec c)] [(inc l) (dec c)]])
 
-(def neighbor-fns [north-neighbors south-neighbors east-neighbors west-neighbors])
+(defn all-neighbors
+  [[l c]]
+  [[(dec l) (dec c)] [(dec l) c] [(dec l) (inc c)]
+   [l (dec c)] [l (inc c)]
+   [(inc l) (dec c)] [(inc l) c] [(inc l) (inc c)]])
+
+(def neighbor-fns [north-neighbors south-neighbors west-neighbors east-neighbors])
 
 (defn rotate
   [[h & t]]
@@ -46,57 +54,100 @@
 
 (defn proposal
   [elves elf neighbor-fns]
-  (let [[x y] elf]
+  (if-not (->> (all-neighbors elf)
+               (filter #(.contains elves %))
+               empty?)
     (if-let [prop (->> neighbor-fns
-                       (map #(% [x y]))
+                       (map #(% elf))
                        (filter (fn [pos] (empty? (filter #(.contains elves %) pos))))
                        first)]
-      (second prop))))
+      (do
+        ;(println elf (second prop))
+        (second prop)))))
 
 (defn moves
-  [elves]
+  [elves neighbor-fns]
   (let [proposals (->> elves
                        (map (fn [elf] [elf (proposal elves elf neighbor-fns)]))
                        (filter second)
                        (into {}))]
+    ;(println (count elves) )
     (filter (fn [[k v]]
               (= 1 (count (filter #(= % v) (vals proposals)))))
             proposals)))
 
 (defn play-n-rounds
   [elves n]
-  (loop [turn  0
-         elves elves]
+  (loop [turn         0
+         elves        elves
+         neighbor-fns neighbor-fns]
     (if (= turn n)
       elves
-      (recur (inc turn) (reduce (fn [elves [from to]] (-> elves (disj from) (conj to))) elves (moves elves))))))
+      (recur
+        (inc turn)
+        (reduce (fn [elves [from to]] (-> elves (disj from) (conj to))) elves (moves elves neighbor-fns))
+        (rotate neighbor-fns)))))
 
 (defn free-spots
   [elves]
-  (let [min-x (->> elves (map first) (apply min))
-        max-x (->> elves (map first) (apply max))
-        min-y (->> elves (map second) (apply min))
-        max-y (->> elves (map second) (apply max))]
-    (println min-x max-x min-y max-y)
+  (let [min-l (->> elves (map first) (apply min))
+        max-l (->> elves (map first) (apply max))
+        min-c (->> elves (map second) (apply min))
+        max-c (->> elves (map second) (apply max))]
+    (println min-l max-l min-c max-c)
     (-
       (*
-        (- (inc max-x) min-x)
-        (- (inc max-y) min-y))
+        (- (inc max-l) min-l)
+        (- (inc max-c) min-c))
       (count elves))))
 
 (defn draw-elves
   [elves]
-  (let [min-x (->> elves (map first) (apply min))
-        max-x (->> elves (map first) (apply max))
-        min-y (->> elves (map second) (apply min))
-        max-y (->> elves (map second) (apply max))]
-    (->> (for [x (range min-x (inc max-x))
-               y (range min-y (inc max-y))]
-           (if (.contains elves [x y])
+  (let [min-l (->> elves (map first) (apply min))
+        max-l (->> elves (map first) (apply max))
+        min-c (->> elves (map second) (apply min))
+        max-c (->> elves (map second) (apply max))]
+    (->> (for [l (range min-l (inc max-l))
+               c (range min-c (inc max-c))]
+           (if (.contains elves [l c])
              \#
              \.))
-         (partition (- (inc max-x) min-x))
+         (partition (- (inc max-c) min-c))
          (map #(apply str %))
-         (map println ))))
+         (mapv println))))
 
-(draw-elves sample-elves)
+(comment
+  (for [i (range 0 11)]
+    (do
+      (println (str "round " i ":"))
+      (draw-elves (play-n-rounds sample-elves i))
+      (println "============"))))
+
+(comment
+  (for [i (range 0 4)]
+    (do
+      (println (str "round " i ":"))
+      (draw-elves (play-n-rounds mini-sample-elves i))
+      (println "============"))))
+
+;part1
+(comment
+  (time
+    (free-spots (play-n-rounds elves 10))))
+
+(defn play-until-end
+  [elves]
+  (loop [turn         0
+         elves        elves
+         neighbor-fns neighbor-fns]
+    (let [next-moves (moves elves neighbor-fns)]
+      (if (empty? next-moves)
+        (inc turn)
+        (recur
+          (inc turn)
+          (reduce (fn [elves [from to]] (-> elves (disj from) (conj to))) elves (moves elves neighbor-fns))
+          (rotate neighbor-fns))))))
+
+(comment 
+  (time 
+    (play-until-end elves)))
