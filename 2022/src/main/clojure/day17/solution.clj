7ee1a0f9fd704e00f222cb4f37f75e98eb8be94c
Dennis Hempfing <dennis.hempfing@sap.com>
2022-12-29 22:56:27 +0100

    add ugly as hell solution for day17

--- 2022/src/main/clojure/day17/solution.clj
+++ 2022/src/main/clojure/day17/solution.clj
@@ -67,9 +67,13 @@
        (map (fn [[x y]] [(inc x) y]))
        (into #{})))
 
+;(defn collides-with-pile?
+;  [^HashSet pile piece]
+;  (reduce #(or %1 %2) (map #(.contains pile %) piece)))
+
 (defn collides-with-pile?
-  [^HashSet pile piece]
-  (reduce #(or %1 %2) (map #(.contains pile %) piece)))
+  [pile piece]
+  (not= 0 (count (intersection (pile->set pile) piece))))
 
 (defn apply-jet
   [jet pile piece]
@@ -115,44 +119,189 @@
 (def sample-inputv (into [] sample-input))
 (def inputv (into [] input))
 
-(time
-  (->> (let [jets  sample-inputv
-             pile  (HashSet.)
-             max-y (atom -1)]
-         (loop [piece-fn-counter 0
-                jet-counter      0
-                piece-counter    0
-                piece            (next-piece (get piece-fns piece-fn-counter) @max-y)]
-           (if (= 0 (mod piece-counter 10000))
-             (println piece-counter))
-           (if (= piece-counter
-                  ;1000000000000
-                  ;100000
-                  2022
-                  ;10
-                  )
-             (inc @max-y) ; the task seems to start counting at 1, not at 0
-             (let [jetted-piece (apply-jet (get jets jet-counter) pile piece)]
-               ;(println (sort piece))
-               ;(println (get jets jet-counter))
-               ;(println (sort jetted-piece))
-               (if (can-fall? pile jetted-piece)
-                 (recur piece-fn-counter (mod (inc jet-counter) (count jets)) piece-counter (fall jetted-piece))
-                 (do
-                   ;(println "zack")
-                   (.addAll pile jetted-piece)
-                   (let [max-piece-y (->> jetted-piece (map second) (apply max))]
-                     ;(println max-piece-y " > " @max-y "?")
-                     (if (> max-piece-y @max-y)
-                       (do 
-                         ;(println "swap!") 
-                         (swap! max-y (fn [_] max-piece-y)))))
-                   (let [new-piece-fn-counter (mod (inc piece-fn-counter) 5)
-                         new-piece            (next-piece (get piece-fns new-piece-fn-counter) @max-y)]
-                     (recur new-piece-fn-counter (mod (inc jet-counter) (count jets)) (inc piece-counter) new-piece))
-                   ))))))
-       ;(reduce union)
-       ;(map second)
-       ;(apply max)
-       ;inc ; the task seems to start counting at 1, not at 0
-       ))
+(defn update-pile
+  [pile piece]
+  (conj pile piece))
+
+;part1
+(comment
+  (time
+    (->> (let [jets  sample-inputv
+               pile  (atom [])
+               max-y (atom -1)]
+           (loop [piece-fn-counter 0
+                  jet-counter      0
+                  piece-counter    0
+                  piece            (next-piece (get piece-fns piece-fn-counter) @max-y)
+                  ]
+             ;(if (= 0 (mod piece-counter 10000))
+             ;  (println piece-counter))
+             (if (= piece-counter
+                    ;1000000000000
+                    ;100000
+                    ;2022
+                    ;10
+                    )
+               (inc @max-y) ; the task seems to start counting at 1, not at 0
+               (let [jetted-piece (apply-jet (get jets jet-counter) @pile piece)]
+                 ;(println (sort piece))
+                 ;(println (get jets jet-counter))
+                 ;(println (sort jetted-piece))
+                 (if (can-fall? @pile jetted-piece)
+                   (recur piece-fn-counter (mod (inc jet-counter) (count jets)) piece-counter (fall jetted-piece))
+                   (do
+                     (swap! pile update-pile jetted-piece)
+                     (let [max-piece-y (->> jetted-piece (map second) (apply max))]
+                       ;(println max-piece-y " > " @max-y "?")
+                       (if (> max-piece-y @max-y)
+                         (do
+                           ;(println "swap!") 
+                           (swap! max-y (fn [_] max-piece-y)))))
+                     (let [new-piece-fn-counter (mod (inc piece-fn-counter) 5)
+                           new-piece            (next-piece (get piece-fns new-piece-fn-counter) @max-y)]
+                       (recur new-piece-fn-counter (mod (inc jet-counter) (count jets)) (inc piece-counter) new-piece))
+                     ))))))
+         ;(reduce union)
+         ;(map second)
+         ;(apply max)
+         ;inc ; the task seems to start counting at 1, not at 0
+         )))
+
+(defn zip
+  [& cols]
+  (partition (count cols) (count cols) (apply interleave cols)))
+
+(defn are-halves-equivalent?
+  [a-pile a-check-at]
+  (let [[first-half second-half] (split-at (/ (count @a-pile) 2) @a-pile)
+        first-half-max-y  (->> first-half
+                               last
+                               (map second)
+                               (apply max))
+        offset-first-half (->> first-half
+                               (map (fn [piece]
+                                      (->> piece
+                                           (map (fn [[x y]]
+                                                  [x (+ y first-half-max-y 1)]))
+                                           (into #{})))))
+        zipped            (zip offset-first-half second-half)]
+    (swap! a-check-at + 10)
+    (->> zipped
+         (map (fn [[p1 p2]] (= p1 p2)))
+         (reduce #(and %1 %2)))
+    ))
+
+(count inputv)
+
+(defn are-halves-equivalent?2
+  [a-pile a-check-at]
+  (swap! a-check-at + 10)
+  (let [offset (->> (get @a-pile (/ (count @a-pile) 2)) (map second) (apply max))]
+    ;(println offset)
+    (loop [i 0]
+      (if (= i (/ (count @a-pile) 2))
+        true
+        (let [offset-a (->> (get @a-pile i) (map (fn [[x y]] [x (+ y offset)])) (into #{}))]
+          ;(println offset-a (get @a-pile (+ i (/ (count @a-pile) 2))))
+          (if (= offset-a (get @a-pile (+ i (/ (count @a-pile) 2))))
+            (recur (inc i))
+            false))))))
+
+;part2
+; idea: collect the amount of pile growth for all five blocks
+(defn growth
+  [five-blocks]
+  (let [flattened (apply union five-blocks)]
+    (- (->> flattened (map second) (apply max)) (->> flattened (map second) (apply min)))))
+
+(defn find-repeating-sequence
+  [col]
+  (let [growths (reverse col)]
+    (loop [i 1]
+      (if (= i (/ (count growths) 2))
+        nil
+        (if (= (take i growths) (->> growths (drop i) (take i)))
+          (take i growths)
+          (recur (inc i)))))))
+
+(comment
+  (time
+    (let [pile                            (let [jets     inputv
+                                                pile     (atom [])
+                                                max-y    (atom -1)
+                                                check-at (atom 10)]
+                                            (loop [piece-fn-counter 0
+                                                   jet-counter      0
+                                                   piece-counter    0
+                                                   piece            (next-piece (get piece-fns piece-fn-counter) @max-y)]
+                                              ;(if (= 0 (mod piece-counter 10000))
+                                              ;  (println piece-counter))
+                                              (if (= piece-counter
+                                                     ;1000000000000
+                                                     5000
+                                                     ;2022
+                                                     ;100
+                                                     )
+                                                ;(inc @max-y) ; the task seems to start counting at 1, not at 0
+                                                @pile
+                                                (let [jetted-piece (apply-jet (get jets jet-counter) @pile piece)]
+                                                  ;(println (sort piece))
+                                                  ;(println (get jets jet-counter))
+                                                  ;(println (sort jetted-piece))
+                                                  (if (can-fall? @pile jetted-piece)
+                                                    (recur piece-fn-counter (mod (inc jet-counter) (count jets)) piece-counter (fall jetted-piece))
+                                                    (do
+                                                      (swap! pile update-pile jetted-piece)
+                                                      (let [max-piece-y (->> jetted-piece (map second) (apply max))]
+                                                        ;(println max-piece-y " > " @max-y "?")
+                                                        (if (> max-piece-y @max-y)
+                                                          (do
+                                                            ;(println "swap!") 
+                                                            (swap! max-y (fn [_] max-piece-y)))))
+                                                      (let [new-piece-fn-counter (mod (inc piece-fn-counter) 5)
+                                                            new-piece            (next-piece (get piece-fns new-piece-fn-counter) @max-y)]
+                                                        (recur new-piece-fn-counter (mod (inc jet-counter) (count jets)) (inc piece-counter) new-piece))
+                                                      ))))))
+          bla (println "piles")
+          growths                         (->> pile
+                                               (partition 5)
+                                               (map growth))
+          rep-seq                         (find-repeating-sequence growths)
+          bla (println rep-seq)
+          bottom-end-count                (->> growths
+                                               reverse
+                                               (partition-all (count rep-seq))
+                                               last
+                                               count
+                                               (* 5))
+          bottom-end-max-y                (->> pile
+                                               (take bottom-end-count)
+                                               (apply union)
+                                               (map second)
+                                               (apply max))
+          bottom-end-plus-one-cycle-max-y (->> pile
+                                               (take (+ bottom-end-count (* 5 (count rep-seq))))
+                                               (apply union)
+                                               (map second)
+                                               (apply max))
+          growth-per-cycle                (- bottom-end-plus-one-cycle-max-y bottom-end-max-y)
+          num-complete-cycles             (quot (- 1000000000000 bottom-end-count) (* 5 (count rep-seq)))
+          remaining-top-end               (rem (- 1000000000000 bottom-end-count) (* 5 (count rep-seq)))
+          remaining-top-end-grwoth        (- (->> pile
+                                                  (take (+ remaining-top-end bottom-end-count))
+                                                  (apply union)
+                                                  (map second)
+                                                  (apply max)
+
+                                                  )
+                                             bottom-end-max-y)
+          ]
+      (+
+        bottom-end-max-y
+        (*
+          growth-per-cycle
+          num-complete-cycles)
+        remaining-top-end-grwoth
+        1 ; because I'm counting from zero
+        )
+      )))
--- 2022/test/main/clojure/day17/solution_test.clj
+++ 2022/test/main/clojure/day17/solution_test.clj
@@ -63,28 +63,55 @@
       (= (->> (block 0) (map first) (apply min)) 2))
     (is
       (= (->> (reverse-l 0) (map first) (apply min)) 2)))
-  (testing "next piece's bottom y is always 4 higher than the top of the pile"
-    (is
-      (= 
-        (->> (next-piece horizontal-line '()) (map second) (apply min))
-        3))
-    (is
-      (=
-        (->> (next-piece horizontal-line '(#{[0 0] [1 0] [2 0] [3 0]})) (map second) (apply min))
-        4))
-    (is
-      (=
-        (->> (next-piece horizontal-line '(#{[0 0] [1 0] [2 0] [2 1] [2 2]})) (map second) (apply min))
-        6))
-    (is
-      (=
-        (->> (next-piece horizontal-line '(#{[0 1] [1 1] [0 2] [2 2]} #{[0 0] [1 0] [2 0] [2 1] [2 2]})) (map second) (apply min))
-        6))
-    (is
-      (=
-        (->> (next-piece plus '(#{[2 0] [3 0] [4 0] [5 0]})) (map second) (apply min))
-        4))
-    (is
-      (=
-        (->> (next-piece reverse-l '(#{[1 2] [2 1] [2 2] [2 3] [3 2]} #{[2 0] [3 0] [4 0] [5 0]})) (map second) (apply min))
-        7))))
+  ;(testing "next piece's bottom y is always 4 higher than the top of the pile"
+  ;  (is
+  ;    (=
+  ;      (->> (next-piece horizontal-line '()) (map second) (apply min))
+  ;      3))
+  ;  (is
+  ;    (=
+  ;      (->> (next-piece horizontal-line '(#{[0 0] [1 0] [2 0] [3 0]})) (map second) (apply min))
+  ;      4))
+  ;  (is
+  ;    (=
+  ;      (->> (next-piece horizontal-line '(#{[0 0] [1 0] [2 0] [2 1] [2 2]})) (map second) (apply min))
+  ;      6))
+  ;  (is
+  ;    (=
+  ;      (->> (next-piece horizontal-line '(#{[0 1] [1 1] [0 2] [2 2]} #{[0 0] [1 0] [2 0] [2 1] [2 2]})) (map second) (apply min))
+  ;      6))
+  ;  (is
+  ;    (=
+  ;      (->> (next-piece plus '(#{[2 0] [3 0] [4 0] [5 0]})) (map second) (apply min))
+  ;      4))
+  ;  (is
+  ;    (=
+  ;      (->> (next-piece reverse-l '(#{[1 2] [2 1] [2 2] [2 3] [3 2]} #{[2 0] [3 0] [4 0] [5 0]})) (map second) (apply min))
+  ;      7)))
+  )
+
+(deftest are-halves-equivalent?-test
+  (is
+    (true?
+      (time (are-halves-equivalent? (atom [#{[3 0] [2 0] [5 0] [4 0]}
+                                     #{[2 2] [3 3] [4 2] [3 1] [3 2]}
+                                     #{[2 3] [2 5] [1 3] [0 3] [2 4]}
+                                     #{[4 3] [4 6] [4 5] [4 4]}
+                                     #{[4 7] [5 7] [4 8] [5 8]}
+                                     #{[3 9] [2 9] [5 9] [4 9]}
+                                     #{[2 11] [3 12] [4 11] [3 10] [3 11]}
+                                     #{[2 12] [2 14] [1 12] [0 12] [2 13]}
+                                     #{[4 12] [4 15] [4 14] [4 13]}
+                                     #{[4 16] [5 16] [4 17] [5 17]}]) (atom 10)))))
+  (is
+    (true?
+      (time (are-halves-equivalent?2 (atom [#{[3 0] [2 0] [5 0] [4 0]}
+                                           #{[2 2] [3 3] [4 2] [3 1] [3 2]}
+                                           #{[2 3] [2 5] [1 3] [0 3] [2 4]}
+                                           #{[4 3] [4 6] [4 5] [4 4]}
+                                           #{[4 7] [5 7] [4 8] [5 8]}
+                                           #{[3 9] [2 9] [5 9] [4 9]}
+                                           #{[2 11] [3 12] [4 11] [3 10] [3 11]}
+                                           #{[2 12] [2 14] [1 12] [0 12] [2 13]}
+                                           #{[4 12] [4 15] [4 14] [4 13]}
+                                           #{[4 16] [5 16] [4 17] [5 17]}]) (atom 10))))))
