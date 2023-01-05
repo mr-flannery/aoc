16e8cc51b57d94c0c28aac8dd6f236e97cd6579b
Dennis Hempfing <dennis.hempfing@sap.com>
2023-01-05 14:41:13 +0100

    add tests for day22 part2

--- 2022/test/main/clojure/day22/solution_test.clj
+++ 2022/test/main/clojure/day22/solution_test.clj
@@ -44,6 +44,12 @@
           (move2 [150 0] :north 10 (first field-and-insts) 200 150)
           [[148 0] :north]))
     (is (=
+          (move2 [150 21] :north 10 (first field-and-insts) 200 150)
+          [[150 21] :north]))
+    (is (=
+          (move2 [153 49] :east 10 (first field-and-insts) 200 150)
+          [[153 49] :east]))
+    (is (=
           (move2 [156 49] :east 10 (first field-and-insts) 200 150)
           [[140 56] :north]))
     (is (=
@@ -54,11 +60,20 @@
           [[199 23] :south]))
     (is (=
           (move2 [174 0] :west 10 (first field-and-insts) 200 150)
-          [[7 74] :south])))
+          [[7 74] :south]))
+    (is (=
+          (move2 [151 0] :west 10 (first field-and-insts) 200 150)
+          [[151 0] :west])))
   (testing "from B"
     (is (=
           (move2 [100 49] :north 10 (first field-and-insts) 200 150)
           [[99 53] :east]))
+    (is (=
+          (move2 [100 46] :north 10 (first field-and-insts) 200 150)
+          [[100 46] :north]))
+    (is (=
+          (move2 [105 49] :east 10 (first field-and-insts) 200 150)
+          [[105 49] :east]))
     (is (=
           (move2 [101 49] :east 10 (first field-and-insts) 200 150)
           [[101 58] :east]))
@@ -70,30 +85,54 @@
           [[149 47] :south]))
     (is (=
           (move2 [100 0] :west 10 (first field-and-insts) 200 150)
+          [[49 56] :east]))
+    (is (=
+          (move2 [100 0] :west 10 (first field-and-insts) 200 150)
           [[49 56] :east])))
   (testing "from C"
     (is (=
           (move2 [100 50] :north 10 (first field-and-insts) 200 150)
           [[97 50] :north]))
     (is (=
+          (move2 [100 93] :north 10 (first field-and-insts) 200 150)
+          [[100 93] :north]))
+    (is (=
           (move2 [100 99] :east 10 (first field-and-insts) 200 150)
           [[49 145] :west]))
+    (is (=
+          (move2 [147 99] :east 10 (first field-and-insts) 200 150)
+          [[147 99] :east]))
     (is (=
           (move2 [149 98] :south 10 (first field-and-insts) 200 150)
           [[198 46] :west]))
     (is (=
+          (move2 [149 54] :south 10 (first field-and-insts) 200 150)
+          [[149 54] :south]))
+    (is (=
           (move2 [149 50] :west 10 (first field-and-insts) 200 150)
-          [[149 40] :west])))
+          [[149 40] :west]))
+    (is (=
+          (move2 [102 50] :west 10 (first field-and-insts) 200 150)
+          [[102 50] :west])))
   (testing "from D"
     (is (=
           (move2 [50 50] :north 10 (first field-and-insts) 200 150)
           [[45 50] :north]))
     (is (=
+          (move2 [50 57] :north 10 (first field-and-insts) 200 150)
+          [[50 57] :north]))
+    (is (=
           (move2 [56 99] :east 10 (first field-and-insts) 200 150)
           [[49 106] :north]))
     (is (=
+          (move2 [94 99] :east 10 (first field-and-insts) 200 150)
+          [[94 99] :east]))
+    (is (=
           (move2 [99 99] :south 10 (first field-and-insts) 200 150)
           [[107 99] :south]))
+    (is (=
+          (move2 [99 69] :south 10 (first field-and-insts) 200 150)
+          [[99 69] :south]))
     (is (=
           (move2 [99 50] :west 10 (first field-and-insts) 200 150)
           [[101 49] :south]))
@@ -105,24 +144,48 @@
           (move2 [0 50] :north 10 (first field-and-insts) 200 150)
           [[150 9] :east]))
     (is (=
+          (move2 [0 99] :north 10 (first field-and-insts) 200 150)
+          [[0 99] :north]))
+    (is (=
           (move2 [49 99] :east 10 (first field-and-insts) 200 150)
           [[49 109] :east]))
+    (is (=
+          (move2 [33 99] :east 10 (first field-and-insts) 200 150)
+          [[33 99] :east]))
     (is (=
           (move2 [49 99] :south 10 (first field-and-insts) 200 150)
           [[57 99] :south]))
     (is (=
+          (move2 [49 88] :south 10 (first field-and-insts) 200 150)
+          [[49 88] :south]))
+    (is (=
           (move2 [49 50] :west 10 (first field-and-insts) 200 150)
-          [[100 9] :east])))
+          [[100 9] :east]))
+    (is (=
+          (move2 [2 50] :west 10 (first field-and-insts) 200 150)
+          [[2 50] :west])))
   (testing "from F"
     (is (=
           (move2 [0 149] :north 10 (first field-and-insts) 200 150)
           [[190 49] :north]))
     (is (=
+          (move2 [0 148] :north 10 (first field-and-insts) 200 150)
+          [[0 148] :north]))
+    (is (=
           (move2 [49 149] :east 10 (first field-and-insts) 200 150)
           [[100 90] :west]))
+    (is (=
+          (move2 [0 149] :east 10 (first field-and-insts) 200 150)
+          [[0 149] :east]))
     (is (=
           (move2 [49 101] :south 10 (first field-and-insts) 200 150)
           [[51 97] :west]))
     (is (=
+          (move2 [49 145] :south 10 (first field-and-insts) 200 150)
+          [[49 145] :south]))
+    (is (=
           (move2 [49 100] :west 10 (first field-and-insts) 200 150)
-          [[49 90] :west]))))
+          [[49 90] :west]))
+    (is (=
+          (move2 [38 100] :west 10 (first field-and-insts) 200 150)
+          [[38 100] :west]))))
